6b67d2ce0e654355a2be4fe193012b3b0211326b
Tony Worm <verdverm@gmail.com>
2020-05-14 01:02:12 -0600

    bump cli version for update fix

--- cmd/dma/cmd/root.go
+++ cmd/dma/cmd/root.go
@@ -32,7 +32,7 @@ func RootPersistentPreRun(args []string) (err error) {
 
 func RootPersistentPostRun(args []string) (err error) {
 
-	PrintUpdateAvailable()
+	WaitPrintUpdateAvailable()
 
 	return err
 }
